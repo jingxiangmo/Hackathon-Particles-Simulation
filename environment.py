ccba90e1af298674369001671e3a492163f4df68
Jingxiang Mo <65676392+Xetile@users.noreply.github.com>
2021-11-06 16:40:02 -0400

    Particle testing in environments

--- environment.py
+++ environment.py
@@ -1,4 +1,5 @@
 from vpython import *
+import particle
 
 animation = canvas(width=800, height=400, align='left')
 gray = color.gray(0.7)
@@ -36,23 +37,114 @@ vert4.append([vector(d, -d, -d), vector(d, d, -d)])
 # ================= Ball Example =================
 #
 
-ball = sphere(color=color.green, radius=0.1, retain=200)
-ball.mass = 1.0
-ball.p = vector(-0.15, -0.23, +0.27)
 
-side = 4.0
-thk = 0.3
-s2 = 2 * side - thk
-s3 = 2 * side + thk
-side = side - thk * 0.5 - ball.radius
+L = 1
 
-dt = 0.3
+mass = 4E-3 / 6E23  # helium mass
+Ratom = 0.03  # wildly exaggerated size of helium atom
+k = 1.4E-23  # Boltzmann constant
+T = 300  # around room temperature
+dt = 1E-5
+Natoms = 200  # change this to have more or fewer atoms
+
+
+Atoms = []
+p = []
+apos = []
+pavg = sqrt(2 * mass * 1.5 * k * T)  # average kinetic energy p**2/(2mass) = (3/2)kT
+
+for i in range(Natoms):
+    x = L * random() - L / 2
+    y = L * random() - L / 2
+    z = L * random() - L / 2
+    if i == 0:
+        Atoms.append(sphere(pos=vector(x, y, z), radius=Ratom, color=color.cyan, make_trail=True, retain=100,
+                            trail_radius=0.3 * Ratom))
+    else:
+        Atoms.append(sphere(pos=vector(x, y, z), radius=Ratom, color=gray))
+    apos.append(vec(x, y, z))
+    theta = pi * random()
+    phi = 2 * pi * random()
+    px = pavg * sin(theta) * cos(phi)
+    py = pavg * sin(theta) * sin(phi)
+    pz = pavg * cos(theta)
+    p.append(vector(px, py, pz))
+
+def checkCollisions():
+    hitlist = []
+    r2 = 2 * Ratom
+    r2 *= r2
+    for i in range(Natoms):
+        ai = apos[i]
+        for j in range(i):
+            aj = apos[j]
+            dr = ai - aj
+            if mag2(dr) < r2: hitlist.append([i, j])
+    return hitlist
+
+
 while True:
-    rate(200)
-    ball.pos = ball.pos + (ball.p / ball.mass) * dt
-    if not (side > ball.pos.x > -side):
-        ball.p.x = -ball.p.x
-    if not (side > ball.pos.y > -side):
-        ball.p.y = -ball.p.y
-    if not (side > ball.pos.z > -side):
-        ball.p.z = -ball.p.z
+    rate(300)
+
+    # Update all positions
+    for i in range(Natoms): Atoms[i].pos = apos[i] = apos[i] + (p[i] / mass) * dt
+
+    # Check for collisions
+    hitlist = checkCollisions()
+
+    # If any collisions took place, update momenta of the two atoms
+    for ij in hitlist:
+        i = ij[0]
+        j = ij[1]
+        ptot = p[i] + p[j]
+        posi = apos[i]
+        posj = apos[j]
+        vi = p[i] / mass
+        vj = p[j] / mass
+        vrel = vj - vi
+        a = vrel.mag2
+        if a == 0: continue;  # exactly same velocities
+        rrel = posi - posj
+        if rrel.mag > Ratom: continue  # one atom went all the way through another
+
+        # theta is the angle between vrel and rrel:
+        dx = dot(rrel, vrel.hat)  # rrel.mag*cos(theta)
+        dy = cross(rrel, vrel.hat).mag  # rrel.mag*sin(theta)
+        # alpha is the angle of the triangle composed of rrel, path of atom j, and a line
+        #   from the center of atom i to the center of atom j where atome j hits atom i:
+        alpha = asin(dy / (2 * Ratom))
+        d = (2 * Ratom) * cos(alpha) - dx  # distance traveled into the atom from first contact
+        deltat = d / vrel.mag  # time spent moving from first contact to position inside atom
+
+        posi = posi - vi * deltat  # back up to contact configuration
+        posj = posj - vj * deltat
+        mtot = 2 * mass
+        pcmi = p[i] - ptot * mass / mtot  # transform momenta to cm frame
+        pcmj = p[j] - ptot * mass / mtot
+        rrel = norm(rrel)
+        pcmi = pcmi - 2 * pcmi.dot(rrel) * rrel  # bounce in cm frame
+        pcmj = pcmj - 2 * pcmj.dot(rrel) * rrel
+        p[i] = pcmi + ptot * mass / mtot  # transform momenta back to lab frame
+        p[j] = pcmj + ptot * mass / mtot
+        apos[i] = posi + (p[i] / mass) * deltat  # move forward deltat in time
+        apos[j] = posj + (p[j] / mass) * deltat
+
+    for i in range(Natoms):
+        loc = apos[i]
+        if abs(loc.x) > L / 2:
+            if loc.x < 0:
+                p[i].x = abs(p[i].x)
+            else:
+                p[i].x = -abs(p[i].x)
+
+        if abs(loc.y) > L / 2:
+            if loc.y < 0:
+                p[i].y = abs(p[i].y)
+            else:
+                p[i].y = -abs(p[i].y)
+
+        if abs(loc.z) > L / 2:
+            if loc.z < 0:
+                p[i].z = abs(p[i].z)
+            else:
+                p[i].z = -abs(p[i].z)
---
+++ particle.py
@@ -0,0 +1,156 @@
+from vpython import *
+
+# GlowScript 3.2 VPython
+
+# Hard-sphere gas.
+
+# Bruce Sherwood
+
+win = 500
+
+Natoms = 200  # change this to have more or fewer atoms
+
+# Typical values
+L = 1  # container is a cube L on a side
+gray = color.gray(0.7)  # color of edges of container
+mass = 4E-3 / 6E23  # helium mass
+Ratom = 0.03  # wildly exaggerated size of helium atom
+k = 1.4E-23  # Boltzmann constant
+T = 300  # around room temperature
+dt = 1E-5
+
+animation = canvas(width=win, height=win, align='left')
+animation.range = L
+animation.title = 'A "hard-sphere" gas'
+
+d = L / 2 + Ratom
+r = 0.005
+boxbottom = curve(color=gray, radius=r)
+boxbottom.append([vector(-d, -d, -d), vector(-d, -d, d), vector(d, -d, d), vector(d, -d, -d), vector(-d, -d, -d)])
+boxtop = curve(color=gray, radius=r)
+boxtop.append([vector(-d, d, -d), vector(-d, d, d), vector(d, d, d), vector(d, d, -d), vector(-d, d, -d)])
+vert1 = curve(color=gray, radius=r)
+vert2 = curve(color=gray, radius=r)
+vert3 = curve(color=gray, radius=r)
+vert4 = curve(color=gray, radius=r)
+vert1.append([vector(-d, -d, -d), vector(-d, d, -d)])
+vert2.append([vector(-d, -d, d), vector(-d, d, d)])
+vert3.append([vector(d, -d, d), vector(d, d, d)])
+vert4.append([vector(d, -d, -d), vector(d, d, -d)])
+
+
+
+
+
+
+
+
+
+mass = 4E-3 / 6E23  # helium mass
+Ratom = 0.03  # wildly exaggerated size of helium atom
+k = 1.4E-23  # Boltzmann constant
+T = 300  # around room temperature
+dt = 1E-5
+Natoms = 200  # change this to have more or fewer atoms
+
+
+Atoms = []
+p = []
+apos = []
+pavg = sqrt(2 * mass * 1.5 * k * T)  # average kinetic energy p**2/(2mass) = (3/2)kT
+
+for i in range(Natoms):
+    x = L * random() - L / 2
+    y = L * random() - L / 2
+    z = L * random() - L / 2
+    if i == 0:
+        Atoms.append(sphere(pos=vector(x, y, z), radius=Ratom, color=color.cyan, make_trail=True, retain=100,
+                            trail_radius=0.3 * Ratom))
+    else:
+        Atoms.append(sphere(pos=vector(x, y, z), radius=Ratom, color=gray))
+    apos.append(vec(x, y, z))
+    theta = pi * random()
+    phi = 2 * pi * random()
+    px = pavg * sin(theta) * cos(phi)
+    py = pavg * sin(theta) * sin(phi)
+    pz = pavg * cos(theta)
+    p.append(vector(px, py, pz))
+
+def checkCollisions():
+    hitlist = []
+    r2 = 2 * Ratom
+    r2 *= r2
+    for i in range(Natoms):
+        ai = apos[i]
+        for j in range(i):
+            aj = apos[j]
+            dr = ai - aj
+            if mag2(dr) < r2: hitlist.append([i, j])
+    return hitlist
+
+
+while True:
+    rate(300)
+
+    # Update all positions
+    for i in range(Natoms): Atoms[i].pos = apos[i] = apos[i] + (p[i] / mass) * dt
+
+    # Check for collisions
+    hitlist = checkCollisions()
+
+    # If any collisions took place, update momenta of the two atoms
+    for ij in hitlist:
+        i = ij[0]
+        j = ij[1]
+        ptot = p[i] + p[j]
+        posi = apos[i]
+        posj = apos[j]
+        vi = p[i] / mass
+        vj = p[j] / mass
+        vrel = vj - vi
+        a = vrel.mag2
+        if a == 0: continue;  # exactly same velocities
+        rrel = posi - posj
+        if rrel.mag > Ratom: continue  # one atom went all the way through another
+
+        # theta is the angle between vrel and rrel:
+        dx = dot(rrel, vrel.hat)  # rrel.mag*cos(theta)
+        dy = cross(rrel, vrel.hat).mag  # rrel.mag*sin(theta)
+        # alpha is the angle of the triangle composed of rrel, path of atom j, and a line
+        #   from the center of atom i to the center of atom j where atome j hits atom i:
+        alpha = asin(dy / (2 * Ratom))
+        d = (2 * Ratom) * cos(alpha) - dx  # distance traveled into the atom from first contact
+        deltat = d / vrel.mag  # time spent moving from first contact to position inside atom
+
+        posi = posi - vi * deltat  # back up to contact configuration
+        posj = posj - vj * deltat
+        mtot = 2 * mass
+        pcmi = p[i] - ptot * mass / mtot  # transform momenta to cm frame
+        pcmj = p[j] - ptot * mass / mtot
+        rrel = norm(rrel)
+        pcmi = pcmi - 2 * pcmi.dot(rrel) * rrel  # bounce in cm frame
+        pcmj = pcmj - 2 * pcmj.dot(rrel) * rrel
+        p[i] = pcmi + ptot * mass / mtot  # transform momenta back to lab frame
+        p[j] = pcmj + ptot * mass / mtot
+        apos[i] = posi + (p[i] / mass) * deltat  # move forward deltat in time
+        apos[j] = posj + (p[j] / mass) * deltat
+
+    for i in range(Natoms):
+        loc = apos[i]
+        if abs(loc.x) > L / 2:
+            if loc.x < 0:
+                p[i].x = abs(p[i].x)
+            else:
+                p[i].x = -abs(p[i].x)
+
+        if abs(loc.y) > L / 2:
+            if loc.y < 0:
+                p[i].y = abs(p[i].y)
+            else:
+                p[i].y = -abs(p[i].y)
+
+        if abs(loc.z) > L / 2:
+            if loc.z < 0:
+                p[i].z = abs(p[i].z)
+            else:
+                p[i].z = -abs(p[i].z)
