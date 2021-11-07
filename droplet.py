6e4b4f0523ac573cb84a5de45e0dd6ff1c9115e3
TahaRha <ahmed.rhaouti@mail.mcgill.ca>
2021-11-06 20:15:10 -0400

    Update droplet.py

--- droplet.py
+++ droplet.py
@@ -1,19 +1,14 @@
 from vpython import *
-from random import *
 
-droplets = []
-NAtoms = 1
+droplet = sphere(radius = 1, pos = vector(-5, 3, 0))
+t = 0
+dt = 0.1
+g = 9.8
+vy= 2
+vx= 2
 
-for i in range(NAtoms):
-    droplets.append(sphere(radius = 0.03, pos = vector(-5, 3, 0)))
-    t = 0
-    dt = 0.1
-    g = 9.8
-    vy= 2
-    vx= 2
-
-for droplet in droplets:
-    rate(50)
+while droplet.pos.y > 0:
+    rate(100)
     ay = -g
     droplet.pos.x += vx * dt
     droplet.pos.y += vy * dt
