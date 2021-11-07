12c978de0fc4fbc54839dc4c67274af9812a34e3
samoula007 <62022815+samoula007@users.noreply.github.com>
2021-11-07 04:05:07 -0500

    Humanly designed human model with normal leg size

--- models.py
+++ models.py
@@ -9,6 +9,13 @@ def person(x, y, z):
     head = sphere(pos=vector(x, y, z), color=color.gray(.6), radius=0.6)
     body = box(pos=vector(x, y - 1.5, z), size=vector(2, 1, 1),
                color=vector(0.72, 0.42, 0), axis=vector(0, 1, 0))
+    # left legg
+    left_leg = cylinder(pos=vector(x, y - 2.5, z - 0.5), radius=0.3,
+                        axis=vector(0, -4, 0), color=color.gray(.6))
+
+    # right legg
+    right_leg = cylinder(pos=vector(x, y - 2.5, z + 0.5), radius=0.3,
+                         axis=vector(0, -4, 0), color=color.gray(.6))
 
     # mask in front of the head
     mask = box(pos=vector(x+1, z, y), size=vector(0.25, 1, 1),
